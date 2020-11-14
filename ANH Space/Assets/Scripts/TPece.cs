62681a5510271aaef696f8a34c2717f148375f90
aidanthephoenix <potatodevaidan@gmail.com>
2020-11-14 16:33:17 -0500

    Tiles And Tile Magic

---
+++ ANH Space/Assets/Scripts/TPece.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using NaughtyAttributes;
+
+public class TPece : MonoBehaviour
+{
+    public KeyCode TestPress;
+    public KeyCode TestPress2;
+    public KeyCode TestPress3;
+
+    Transform PosRef;
+    Rigidbody Rigg;
+    public bool isstopped;
+    TSpawner SpawnRef;
+    public float fallspeed;
+    public float resetpoint;
+    [Space]
+    public float widthRight;
+    public float widthLeft;
+    public float heighttop;
+    public float heightbottom;
+    public float checkDistRight;
+    public float checkDistLeft;
+    public float checkDistDown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PosRef = GetComponent<Transform>();
+        Rigg = GetComponent<Rigidbody>();
+        SpawnRef = FindObjectOfType<TSpawner>();
+        
+        if(Rigg.velocity == Vector3.zero)
+        {
+            Rigg.velocity = -Vector3.up;
+        }
+
+        Rigg.AddRelativeForce(new Vector3(0, -fallspeed, 0), ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(TestPress))
+        {
+            RotateClockwise();
+        }
+        if (Input.GetKeyDown(TestPress2))
+        {
+            MoveRight();
+        }
+        if (Input.GetKeyDown(TestPress3))
+        {
+            MoveLeft();
+        }
+
+        if(transform.position.x != resetpoint)
+        {
+            transform.position = new Vector3(resetpoint, transform.position.y, transform.position.z);
+        }
+
+        print(Mathf.Round(transform.eulerAngles.z));
+
+        if(Mathf.Round(transform.eulerAngles.z) == 0)
+        {
+            Debug.Log("dada");
+            checkDistRight = widthRight;
+            checkDistLeft = widthLeft;       
+        }
+        else if (Mathf.Round(transform.eulerAngles.z) == 270)
+        {
+            Debug.Log("dede");
+            checkDistRight = heighttop;
+            checkDistLeft = heightbottom;
+        }
+        else if (Mathf.Round(transform.eulerAngles.z) == 180)
+        {
+            Debug.Log("dodo");
+            checkDistRight = widthLeft;
+            checkDistLeft = widthRight;
+        }
+        else if (Mathf.Round(transform.eulerAngles.z) == 90)
+        {
+            Debug.Log("dudu");
+            checkDistRight = heightbottom;
+            checkDistLeft = heighttop;
+        }
+        
+        if(transform.localRotation.z >= 360)
+        {
+            PosRef.eulerAngles = new Vector3(0, 0, PosRef.eulerAngles.z - 360);
+        }
+        else if(transform.localRotation.z <= -360)
+        {
+            PosRef.eulerAngles = new Vector3(0, 0, PosRef.eulerAngles.z + 360);
+        }
+
+        if (Rigg.velocity.magnitude <= .1 && isstopped == false)
+        {
+            Rigg.useGravity = true;
+            Rigg.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            isstopped = true;
+            SpawnRef.Spawn();
+        }
+    }
+
+    [Button("RotateClockwise")]
+    void RotateClockwise()
+    {     
+        if(isstopped == false)
+        {
+            PosRef.eulerAngles = new Vector3(0, 0, PosRef.eulerAngles.z + 90);
+        }
+    }
+
+    [Button("RotateCounterclockwise")]
+    void RotateCounterclockwise()
+    {
+        if (isstopped == false)
+        {
+            PosRef.eulerAngles = new Vector3(0, 0, PosRef.eulerAngles.z - 90);
+        }
+    }
+
+    [Button("MoveRight")]
+    void MoveRight()
+    {
+        if (isstopped == false)
+        {
+            RaycastHit hit;
+            Debug.DrawRay(transform.position + (Vector3.right * checkDistRight), Vector3.right,Color.red,10f);
+            if(Physics.Raycast(transform.position + (Vector3.right * checkDistRight), Vector3.right, out hit, 1))
+            {
+                Debug.Log("Hit something");
+            }
+            else
+            {
+                resetpoint ++;
+                PosRef.position = new Vector3(PosRef.position.x + 1, PosRef.position.y, PosRef.position.z);
+            }
+        }
+    }
+
+    [Button("MoveLeft")]
+    void MoveLeft()
+    {
+        if (isstopped == false)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position - (Vector3.right * checkDistLeft), -Vector3.right, out hit, 1))
+            {
+                Debug.Log("Hit something");
+            }
+            else
+            {
+                resetpoint--;
+                PosRef.position = new Vector3(PosRef.position.x - 1, PosRef.position.y, PosRef.position.z);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.right));
+    }
+}
---
+++ ANH Space/Assets/Scripts/TSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSpawner : MonoBehaviour
+{
+    public List<GameObject> TilesPossible = new List<GameObject>();
+    public GameObject ActiveTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void Spawn()
+    {
+            GameObject gg = Instantiate(TilesPossible[Random.Range(0, TilesPossible.Count)], transform.position, Quaternion.identity);
+            ActiveTile = gg;
+    }
+}
