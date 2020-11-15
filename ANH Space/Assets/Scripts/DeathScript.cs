20ed942d9ab22f43ff8986377ab64f7d68b63c26
guizx <guilherme.santos2010@hotmail.com>
2020-11-15 14:59:25 -0300

    ITS THE FINAL COUNTDOWN

---
+++ ANH Space/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            Debug.Log("TU MORREU");
+        }
+    }
+}
---
+++ ANH Space/Assets/Scripts/WinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("WinScreen");
+            Debug.Log("YOU WON");
+        }
+    }
+
+}
